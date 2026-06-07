f1f5b5d664dbe21b438a48803e44f571b1cfa52d
Albert <alberthichez605@gmail.com>
2026-06-06 23:08:29 -0400

    match_s5 practica

--- match_s5.py
+++ match_s5.py
@@ -187,5 +187,100 @@ os.system("cls")
 #     case _:
 #         ("Rol no autorizado.")
 
+#-----------------------------------------------------------------------------
 
+#ASIGNACION #2 (5PTS. FECHA LIMITE: 15/06/2026)
 
+#Ejercicio 1 – Impuesto progresivo
+
+# ingreso_mensual = int(input("Cual es su ingreso mensual? "))
+
+# if ingreso_mensual > 0:
+#     if ingreso_mensual <= 20000:
+#         print('Exento')
+#     elif 20001 <= ingreso_mensual <= 40000:
+#         print(f"Se le calcula 10% de impuesto: {ingreso_mensual * 0.10}")
+#     else:
+#         print(f"Se le calcula 20% de impuesto: {ingreso_mensual * 0.20}")
+# else:
+#     print('El ingreso debe ser mayor a 0')
+
+#-----------------------------------------------------------------------------
+
+#Ejercicio 2 – Plan telefónico
+
+# plan_telefonico = input('Ingrese el tipo de plan deseado: Basico, Estandar o Premium\n')
+
+# match plan_telefonico:
+#     case "Basico": print('''El plan basico tiene un costo de: RD$500
+# beneficios: 5 GB de internet, 100 minutos de llamadas y 100 SMS.''')
+#     case "Estandar": print('''El plan basico tiene un costo de: RD$900
+# beneficios: 15 GB de internet, llamadas ilimitadas nacionales y 500 SMS.''')
+#     case "Premium":  print('''El plan basico tiene un costo de: RD$1,500
+# beneficios: Internet ilimitado, llamadas ilimitadas nacionales e internacionales y SMS ilimitados.''')
+#     case _:
+#         print("El plan no existe, favor de ingresar uno valido")
+        
+#-----------------------------------------------------------------------------
+
+#Ejercicio 3 – Mayor de tres números
+
+# num1 = int(input('Ingrese un primer numero: '))
+# num2 = int(input('Ingrese un segundo numero: '))
+# num3 = int(input('Ingrese un tercer numero: '))
+# resultado = (num1 + num2 + num3) % 2
+# suma = num1 + num2 + num3
+
+# if num1 > num2 and num1 > num3:
+#     print('El primer número es mayor que los demás')
+# elif num2 > num1 and num2 > num3:
+#     print('El segundo número es mayor que los demás')
+# elif num3 > num1 and num3 > num2:
+#     print('El tercer número es mayor que los demás')
+
+# if num1 == num2 == num3:
+#     print('Todos son iguales')
+# elif num1 == num2 or num2 == num3 or num1 == num3:
+#     print('Hay empate de dos números iguales')
+
+# if resultado != 0: 
+#     print(f'La suma de los 3 numeros es: {suma}')
+
+#-----------------------------------------------------------------------------
+
+#Ejercicio 4 – Validación de contraseña
+
+# contraseña = input('Ingrese una contraseña: ')
+# tiene_numero = False 
+# tiene_mayuscula = False 
+
+# for caracter in contraseña: 
+#     if caracter.isdigit():
+#         tiene_numero = True
+#     elif caracter.upper():
+#         tiene_mayuscula = True
+
+# if len(contraseña) >= 8 and tiene_numero and tiene_mayuscula: 
+#     print('contraseña valida')
+# else:
+#     print('La contraseña debe tener al menos 8 caracteres y un numero y una mayuscula')
+
+#-----------------------------------------------------------------------------
+
+#Ejercicio 5 – Clasificación de triángulos
+
+# lad1 = float(input('Ingrese la longitud del primer lado: ')) 
+# lad2 = float(input('Ingrese la longitud del segundo lado: '))
+# lad3 = float(input('Ingrese la longitud del tercer lado: '))
+
+# if lad1 + lad2 > lad3 and lad1 + lad3 > lad2 and lad2 + lad3 > lad1:
+#     print('Pueden formar un triángulo')
+
+#     if lad1 == lad2 == lad3:
+#         print('Este es un triangulo de tipo: Equilatero')
+#     elif lad1 == lad2 or lad2 == lad3 or lad1 == lad3:
+#         print('Este es un triangulo de tipo: Isosceles')
+#     else:
+#         print('Este es un triangulo de tipo: Escaleno')
+# else:
+#     print('No pueden formar un triángulo')
